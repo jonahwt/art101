2b0a10a26e708d7aab5579b95b95b3ee2e906fe0
jonahwt <jonahteitz@gmail.com>
2023-10-22 21:34:05 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -2,6 +2,35 @@
 // Author: Your Name
 // Date:
 
+
+// Create a grid and walls
+// Place the snake and the apple on the grid
+// Set the initial snake direction
+
+// Main game loop
+
+// Print the grid and walls
+
+// Draw the snake, differentiating between the head and tail
+
+// Accept user input and validate it
+    //Have snake move in user direction
+
+// Test if the snake ate the apple
+    // If so, delete the apple, place a new apple on the grid, and add a pixel to the end of the snake
+    // Otherwise, keep the apple in the same spot and do not increase the snake length
+
+// Test for lose condition
+    // If the snake head hits its own body or a wall, the game is lost
+
+// Test for win condition
+    // If the entire grid is filled, the game is won
+
+// If the game is not over, continue the loop
+
+
+
+
 // Constants
 
 // Functions
