f30a4be0c79358ae50e810892696a95d5eba75c6
jonahwt <jonahteitz@gmail.com>
2023-10-26 18:49:44 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - For Lab 5, description of a car
+// Author: Jonah Teitz
+// Date: October 24
+
+//Define Variables
+var make = "Ford";
+var model = "Taurus";
+var color = "Black";
+var year = 1995;
+var currentYear = 2023;
+
+//Calculate
+var age = currentYear - year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: "+ model + "<br>");
+document.writeln("Color: " + color +"<br>");
+document.writeln("year: " + year +"<br>");
+document.writeln("Age: " + age + "<br>");
 
 // Constants
 
