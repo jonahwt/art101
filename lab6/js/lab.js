8648bc0c0191e99f54459ba8393c781abd48cc6f
jonahwt <jonahteitz@gmail.com>
2023-10-29 12:45:27 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Jonah Teitz
+// Date: 10/27/2023
+
+//Variables
+var myTransport = [" legs", " bus", " bike", " 1995 Ford Taurus"];
+
+//Objects
+myMainRide = {
+  make: "Schwinn",
+  model: "leToure",
+  color: "gold",
+  year: 1979,
+  extras: "fixies, Brooks saddle",
+}
+
+//Output
+//For myTransport
+document.writeln("I commonly use these forms of transportation: " + myTransport +".<br>");
+
+//For myMainRide
+document.writeln("<br>"
+)
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+//I had problems having the script run on the website, but when I added this code from lab 5
+//the script starter working.
+var make = "Ford";
+var model = "Taurus";
+var color = "Black";
+var year = 1995;
+var currentYear = 2023;
+
+
 
 // Constants
 
