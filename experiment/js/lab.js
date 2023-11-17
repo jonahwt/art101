4d5fc0cbf702d0f0622d9e28b792e8fbc6cedc6f
jonahwt <jonahteitz@gmail.com>
2023-11-16 21:46:09 -0800

    lab10

---
+++ experiment/js/lab.js
@@ -0,0 +1,10 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+$('#my-button').click(function() {
+  var name = prompt('Name please:');
+  
+  $('#title').html('Hello ' + name + '!');
+})
+
